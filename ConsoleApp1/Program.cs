267917bf4fedfe6e201c5aa75bfc7ddfce5ea489
Vladislav Plotnikov <vadimovich74@gmail.com>
2021-09-15 08:06:53 +0300

    asdfgbnm

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello!");
             //Something wrong!!!!
+            //wrong again!!!
             Console.ReadLine();
         }
     }
