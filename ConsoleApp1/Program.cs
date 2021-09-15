13d09fbc97aa8e327891ef7d38c49004889aba37
Vladislav Plotnikov <vadimovich74@gmail.com>
2021-09-15 08:00:34 +0300

    Added a comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
+            //Something wrong!!!!
             Console.ReadLine();
         }
     }
